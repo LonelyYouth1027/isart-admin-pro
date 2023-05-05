10d04c60c72175ffb06ced5bd40d79bf99a818f7
sym <3162531699@qq.com>
2023-05-05 17:49:21 +0800

    fix: 优化生产环境下打包配置

--- config/vite.config.prod.ts
+++ config/vite.config.prod.ts
@@ -1,54 +1,54 @@
 import { mergeConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
+// import eslint from 'vite-plugin-eslint';
 import baseConfig from './vite.config.base';
-// import configCompressPlugin from './plugin/compress';
-// import configVisualizerPlugin from './plugin/visualizer';
-// import configArcoResolverPlugin from './plugin/arcoResolver';
-// import configStyleImportPlugin from './plugin/styleImport';
-// import configImageminPlugin from './plugin/imagemin';
-
-// export default mergeConfig(
-//   {
-//     mode: 'production',
-//     plugins: [
-//       configCompressPlugin('gzip'),
-//       configVisualizerPlugin(),
-//       configArcoResolverPlugin(),
-//       configStyleImportPlugin(),
-//       configImageminPlugin(),
-//     ],
-//     build: {
-//       rollupOptions: {
-//         output: {
-//           manualChunks: {
-//             arco: ['@arco-design/web-vue'],
-//             chart: ['echarts', 'vue-echarts'],
-//             vue: ['vue', 'vue-router', 'pinia', '@vueuse/core', 'vue-i18n'],
-//           },
-//         },
-//       },
-//       chunkSizeWarningLimit: 2000,
-//     },
-//   },
-//   baseConfig
-// );
+import configCompressPlugin from './plugin/compress';
+import configVisualizerPlugin from './plugin/visualizer';
+import configArcoResolverPlugin from './plugin/arcoResolver';
+import configStyleImportPlugin from './plugin/styleImport';
+import configImageminPlugin from './plugin/imagemin';
 
 export default mergeConfig(
   {
     mode: 'production',
-    server: {
-      open: true,
-      fs: {
-        strict: true,
-      },
-    },
     plugins: [
-      eslint({
-        cache: false,
-        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
-        exclude: ['node_modules'],
-      }),
+      configCompressPlugin('gzip'),
+      configVisualizerPlugin(),
+      configArcoResolverPlugin(),
+      configStyleImportPlugin(),
+      configImageminPlugin(),
     ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            arco: ['@arco-design/web-vue'],
+            chart: ['echarts', 'vue-echarts'],
+            vue: ['vue', 'vue-router', 'pinia', '@vueuse/core', 'vue-i18n'],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 2000,
+    },
   },
   baseConfig
 );
+
+// export default mergeConfig(
+//   {
+//     mode: 'production',
+//     server: {
+//       open: true,
+//       fs: {
+//         strict: true,
+//       },
+//     },
+//     plugins: [
+//       eslint({
+//         cache: false,
+//         include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+//         exclude: ['node_modules'],
+//       }),
+//     ],
+//   },
+//   baseConfig
+// );
