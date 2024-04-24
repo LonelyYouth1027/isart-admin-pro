e99dfc9aa2ccc7e804b64e371deb220f10eed29a
sym <3162531699@qq.com>
2024-04-24 15:17:45 +0800

    fix: 修改配置

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -26,7 +26,7 @@ export default mergeConfig(
       }),
       createSvgIconsPlugin({
         // 指定需要缓存的图标文件夹
-        iconDirs: [path.resolve(process.cwd(), 'src/assets/svgs')],
+        iconDirs: [path.resolve(process.cwd(), 'src/icons')],
         // 指定symbolId格式
         symbolId: 'icon-[dir]-[name]',
       }),
